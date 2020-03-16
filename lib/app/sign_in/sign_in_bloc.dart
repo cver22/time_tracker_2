1cd09d6f88fb6833988d86649f6faf6a9225e9fd
Chris Vervaeke <chris.vervaeke@gmail.com>
2020-03-15 19:45:09 -0700

    Revert "Refactored services to bloc from sign in page"
    This reverts commit ca395ffc

--- lib/app/sign_in/sign_in_bloc.dart
+++ lib/app/sign_in/sign_in_bloc.dart
@@ -1,38 +1,12 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
-import 'package:time_tracker_2/services/auth.dart';
 
-class SignInBloc {
-  SignInBloc({@required this.auth});
-
-  final AuthBase auth;
-  final StreamController<bool> _isLoadingController = StreamController<bool>();
-
-  Stream<bool> get isLoadingStream => _isLoadingController.stream;
+class SignInBloc{
+  final StreamController<bool> _isloadingController = StreamController<bool>();
+  Stream<bool> get isLoadingStream => _isloadingController.stream;
 
   void dispose() {
-    _isLoadingController.close();
+    _isloadingController.close();
   }
 
-  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
-
-  Future<User> _signIn(Future<User> Function() signInMethod) async {
-    try {
-      _setIsLoading(true);
-      //use the () to call the method
-      return await signInMethod();
-    } catch (e) {
-      //passes error back to call
-      rethrow;
-    } finally {
-      _setIsLoading(false);
-    }
-  }
-
-  Future<User> signInAnonymously() async {
-    //passing without the () only passes the method, does not call it
-    return await _signIn(auth.signInAnonymously);
-  }
-
-  Future<User> signInWithGoogle() async => await _signIn(auth.signInWithGoogle);
+  void setIsLoading(bool isLoading) => _isloadingController.add(isLoading);
 }
